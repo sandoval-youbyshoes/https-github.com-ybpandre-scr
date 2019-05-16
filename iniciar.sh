c1559404c1719c1c103562eaa19ab41e664d9cf2
Andre <andre.cordeiro@youbyshoes.com.br>
2019-05-16 12:53:15 -0300

    script iniciar no server you

---
+++ iniciar.sh
@@ -0,0 +1,7 @@
+echo "Iniciando Drive
+
+/bin/su -c "google-drive-ocalmfuse -o nonempty /home/you/grive" - you
+
+echo "Subindo Robo server"
+
+/bin/su -c "forever start /home/shell-bot/server.js" - you
